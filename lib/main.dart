c3a44f33d31d520dd41c099282b0a4074c9eeeb9
soumi guria <soumiguria.cse2022@nsec.ac.in>
2024-02-03 22:59:34 +0530

    main file updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,235 @@
+import 'dart:convert';
+import 'package:diversion/APIEndpoints.dart';
+import 'package:diversion/FutureAnalysis.dart';
+import 'package:diversion/SetupDetails.dart';
+import 'package:diversion/SplashScreen.dart';
+import 'package:diversion/Technologies.dart';
+import 'package:diversion/WinningStrategies.dart';
+import 'package:diversion/api_key.dart';
+import 'package:diversion/components/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await dotenv.load();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ChatProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Project Ideation',
+      home: SplashScreen(),
+
+       routes: {
+    '/Technologies Information': (context) => Technologies(),
+    '/API Endpoints Information': (context) => ApiEndpoints(),
+    '/Setup Details Information': (context) => SetupDetails(),
+    '/Winning Strategies Information': (context) => WinningStrategies(),
+  },
+
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  final List<Message> _messages = [];
+  final TextEditingController _textEditingController = TextEditingController();
+  bool generatedResponse = false;
+  String responseFromGPT = '';
 
-  void _incrementCounter() {
+  void onSendMessage(BuildContext context) async {
+    Message userMessage = Message(
+      text: _textEditingController.text,
+      isMe: true,
+    );
+    _textEditingController.clear();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _messages.insert(0, userMessage);
     });
+
+    String response = await sendMessageToChatGpt(userMessage.text);
+
+    Message chatGpt = Message(text: response, isMe: false);
+
+    setState(() {
+      _messages.insert(0, chatGpt);
+      generatedResponse = true;
+      responseFromGPT = response;
+    });
+
+    ChatProvider chatProvider = Provider.of<ChatProvider>(context, listen: false);
+    chatProvider.setUserPrompt(userMessage.text);
+
+  }
+
+    Future<String> sendMessageToChatGpt(String userMessage) async {
+    String backgroundPrompt = "See you will be a model that will give answers to only a prompt when it is regarding any particular project idea which the user wants to build, assist that user with this product idea  and anything in relevance to the particular project else say \"I don't know the thing what you are asking for, but would surely love to learn about it\".the first line that should be displayed on generating the prompt should always contain an encouraging statement also tells that given idea is unique or not or already similar things are available in market and show but we are here to help you proceed give this in 100 words. Remember not to answer any thing apart from proect idea related things. Don't answer any coding question, nothing irrevent to project ideas, or startups, if that is the case I am going to behave harshly with you";
+
+    Uri uri = Uri.parse("https://api.openai.com/v1/chat/completions");
+
+    Map<String, dynamic> body = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {"role": "system", "content": backgroundPrompt},
+        {"role": "user", "content": userMessage}
+      ],
+    };
+
+    final response = await http.post(
+      uri,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ${APIKey.apiKey}",
+      },
+      body: json.encode(body),
+    );
+
+    Map<String, dynamic> parsedResponse = json.decode(response.body);
+
+    String reply = parsedResponse['choices'][0]['message']['content'];
+
+    return reply;
+  }
+
+  void onProceed() {
+    if (generatedResponse) {
+      // Navigate to the future analysis page and pass the response
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => FutureAnalysisPage(response: responseFromGPT),
+        ),
+      );
+    }
+  }
+
+  Widget _buildMessage(Message message) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.green, Colors.yellow],
+          ),
+        ),
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child: Column(
+          crossAxisAlignment:
+              message.isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              message.isMe ? 'You' : 'Alt Explorer',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(message.text),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Alt Explorer'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF4CAF50), Color(0xFF2196F3)],
+          ),
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              child: ListView.builder(
+                reverse: true,
+                itemCount: _messages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return _buildMessage(_messages[index]);
+                },
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        controller: _textEditingController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(10.0),
+                          hintText: 'Enter your project idea prompt here...',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      
+                      // onPressed: generatedResponse ? onProceed : onSendMessage as void Function(),
+
+                      // onPressed: generatedResponse ? onProceed : onSendMessage,
+
+                      onPressed: generatedResponse ? () => onProceed() : () => onSendMessage(context),
+
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue.shade800,
+                        side: BorderSide(color: Colors.white, width: 2.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 10.0),
+                        child: Text(
+                          generatedResponse ? 'Proceed' : 'Generate',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Message {
+  final String text;
+  final bool isMe;
+
+  Message({required this.text, required this.isMe});
 }
